bcc3aff6c5350c2b35a0229d9636c3f375a6f563
HENQ <corrqui@gmail.com>
2026-02-11 00:44:43 +0900

    1

--- spt/index.js
+++ spt/index.js
@@ -1541,7 +1541,7 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/index_atlas_1.png?1770737251558", id:"index_atlas_1"}
+		{src:"images/index_atlas_1.png?1770738266196", id:"index_atlas_1"}
 	],
 	preloads: []
 };
